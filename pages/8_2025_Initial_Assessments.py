7a394f782354fac7dc60d4631c8c93cff568f5d4
Jim McKeown <mckeown.james@gmail.com>
2025-03-11 18:52:16 -0400

    mar 11 data

--- pages/8_2025_Initial_Assessments.py
+++ pages/8_2025_Initial_Assessments.py
@@ -40,8 +40,8 @@ st.title("2025 Initial Assessments")
 # Read and process data
 try:
     df, df_ecd = load_egra_data(
-        children_filename="EGRA form-assessment_repeat - Feb 19.csv",
-        ta_filename="EGRA form - Feb 19.csv"
+        children_filename="EGRA form [Eastern Cape]-assessment_repeat - Mar 11.csv",
+        ta_filename="EGRA form [Eastern Cape] - Mar 11.csv"
     )
 
     # START OF PAGE
